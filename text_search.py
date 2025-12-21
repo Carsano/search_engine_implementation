8fe79dd8e7b23c7385265d7afae0a10af2b9e8f6
Carsano <damien.fabiano@protonmail.com>
2025-12-21 12:22:08 +0100

    create text_search class

---
+++ text_search.py
@@ -0,0 +1,13 @@
+"""
+Docstring pour text_search
+"""
+
+import pandas as pd
+import numpy as np
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+class TextSearch:
+    def __init__(self):
+        pass
